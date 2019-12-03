2add22f2409127361472136460f9854409af349e
70yamanaka <nao.ktm.12.25@icloud.com>
2019-12-03 18:29:11 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
     function buildHTML(message){        
-        var img = (message.image_url) ? `<img class="lower-message__image" src="${ message.image_url }">` : "";
-        var html = `<div class="message" data-message_id="16227">
+        var img = (message.image) ? `<img class="lower-message__image" src="${ message.image }">` : "";
+        var html = `<div class="message" data-message_id="${message.id}">
                         <div class="message__upper-info">
                             <p class="message__upper-info__talker">
                                 ${message.user_name}
@@ -40,6 +40,29 @@ $(function(){
         })
         .fail(function(){
             alert("メッセージ送信に失敗しました");
-        })
-    })
+        });
+    });
+    
+        var reloadMessages = function() {
+            if (window.location.href.match(/\/groups\/\d+\/messages/))
+                var last_message_id = $('.message:last').data("message-id");
+            $.ajax({
+            url: "api/messages",
+            type: 'get',
+            dataType: 'json',
+            data: {id: last_message_id}
+            })
+            .done(function(messages) {
+                var insertHTML = '';
+                $.each(messages, function(i, message) {
+                    insertHTML += buildHTML(message)
+                });
+                $('.messages').append(insertHTML);
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+            })
+            .fail(function() {
+            console.log('error');
+            })
+        }
+    setInterval(reloadMessages, 7000);
 });
