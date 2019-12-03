4051f4c88966e18f06a36d177b374def452ed1c7
70yamanaka <nao.ktm.12.25@icloud.com>
2019-12-03 18:51:46 +0900

    自動更新　alert追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,7 +61,7 @@ $(function(){
                 $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
             })
             .fail(function() {
-            console.log('error');
+                alert('自動更新に失敗しました');
             })
         }
     setInterval(reloadMessages, 7000);
