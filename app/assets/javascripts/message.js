2016a7c82218ecae0aceae5be3332a3e65168c38
70yamanaka <nao.ktm.12.25@icloud.com>
2019-12-06 11:48:41 +0900

    setting s3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,7 +45,7 @@ $(function(){
     
         var reloadMessages = function() {
             if (window.location.href.match(/\/groups\/\d+\/messages/)){
-                var last_message_id = $('.message:last').data("message-id");
+                var last_message_id = $('.message:last').data("message_id");
             $.ajax({
             url: "api/messages",
             type: 'get',
